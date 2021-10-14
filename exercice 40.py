53a05bc558dc77f5f6ce7de1a62647a32257868f
TREMOUILLEThomas <90332661+TREMOUILLEThomas@users.noreply.github.com>
2021-10-14 16:39:41 +0200

    Add files via upload

---
+++ exercice 40.py
@@ -0,0 +1,10 @@
+a = int ( input ("la boule 1 à renverser combien de quilles ? :"))
+if a==10:
+    print ("STRICK !")
+elif a<10:
+    int(input("la boule 2 à renverser combien de quilles ? :"))
+b=a
+if b==10:
+    print ("STRICK !")
+elif b<10:
+    print ("PERDU !")
